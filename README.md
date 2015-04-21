d27a23e562ba16228835f6b095905530ac41d6fa
Christian Kaiser <ck@naxio.ch>
2015-04-21 14:59:47 +0200

    included example for Leaflet and JS config file

--- README.md
+++ README.md
@@ -4,4 +4,19 @@ Different marker icons for interactive mapping, e.g. for use with Leaflet or Goo
 
 Marker icons are simple PNG images. AI files are also included if you want to modify the markers.
 
+Here is a simple usage of the icon for Leaflet:
+
+    var blueHotelIcon = L.icon({
+        iconUrl: 'https://github.com/christiankaiser/map-marker-icons/raw/master/icons/plain-blue-hotel.png',
+        iconSize: [32, 41],
+        iconAnchor:   [16, 40],
+        popupAnchor:  [0, -40]
+     });
+    
+    L.marker([46.18, 7.15], {icon: blueHotelIcon}).addTo(map).bindPopup('Sweet dreams hotel');
+
+If you want to include the `leaflet-map-marker-icons.js` file into your code, you can create the same marker as follows:
+
+	var blueHotelIcon = L.icon(mapMarkerIcons.plainBlueHotel);
+	L.marker([46.18, 7.15], {icon: blueHotelIcon}).addTo(map).bindPopup('Sweet dreams hotel');
 
---
+++ leaflet-map-marker-icons.js
@@ -0,0 +1,26 @@
+mapMarkerIcons = {
+    'plainBlue': {
+        iconUrl: 'https://github.com/christiankaiser/map-marker-icons/raw/master/icons/plain-blue.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 40],
+        popupAnchor: [0, -40]
+    },
+    'plainRed': {
+        iconUrl: 'https://github.com/christiankaiser/map-marker-icons/raw/master/icons/plain-red.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 40],
+        popupAnchor: [0, -40]
+    },
+    'plainBlueHotel': {
+        iconUrl: 'https://github.com/christiankaiser/map-marker-icons/raw/master/icons/plain-blue-hotel.png',
+        iconSize: [32, 41],
+        iconAnchor: [16, 40],
+        popupAnchor: [0, -40]
+    },
+    'plainRedHotel': {
+        iconUrl: 'https://github.com/christiankaiser/map-marker-icons/raw/master/icons/plain-red-hotel.png',
+        iconSize: [32, 41],
+        iconAnchor: [16, 40],
+        popupAnchor: [0, -40]
+    }
+}
